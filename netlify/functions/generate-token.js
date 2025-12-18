b3d491c05c91bbff8d5393d891e738b61cc14a3d
_.yzero <116497775+FISTOFDARKNESS@users.noreply.github.com>
2025-12-18 17:33:56 +0100

    Update generate-token.js

--- netlify/functions/generate-token.js
+++ netlify/functions/generate-token.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 const SECRET_KEY = "zeta-realm-master-secret-key-2024";
-const SCRIPT_CONTENT = `https://pastefy.app/RBcneQxu/raw`;
+const SCRIPT_CONTENT = `loadstring(game:HttpGet("https://pastefy.app/RBcneQxu/raw"))()`;
 
 exports.handler = async (event) => {
     const headers = {
