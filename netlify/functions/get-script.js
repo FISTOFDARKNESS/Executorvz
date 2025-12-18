da3dcffc9c08a028d8c0f5f95a2acf52b9dfcf22
_.yzero <116497775+FISTOFDARKNESS@users.noreply.github.com>
2025-12-18 15:28:57 +0100

    Refactor get-script.js to return a script response

---
+++ netlify/functions/get-script.js
@@ -0,0 +1,15 @@
+exports.handler = async (event) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Content-Type": "text/plain"
+  };
+
+  const script = `print("hello")`;
+
+  return {
+    statusCode: 200,
+    headers,
+    body: script
+  };
+};
